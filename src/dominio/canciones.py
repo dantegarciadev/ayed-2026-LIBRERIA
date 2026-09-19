8823db951155a8737840d3534b39af20920a8cca
samaniegopy <samaniego.py04@gmail.com>
2026-09-19 18:55:00 -0300

    Update canciones.py

--- src/dominio/canciones.py
+++ src/dominio/canciones.py
@@ -1,49 +1,30 @@
 CATALOGO = [
-    {
-        "id": "1",
-        "titulo": "De Musica Ligera",
-        "artista": "Soda Stereo",
-        "album": "Cancion Animal",
-        "genero": "Rock",
-        "anio": 1990,
-        "duracion_seg": 213
-    },
-    {
-        "id": "2",
-        "titulo": "Persiana Americana",
-        "artista": "Soda Stereo",
-        "album": "Signos",
-        "genero": "Rock",
-        "anio": 1986,
-        "duracion_seg": 263
-    },
-    {
-        "id": "3",
-        "titulo": "En la Ciudad de la Furia",
-        "artista": "Soda Stereo",
-        "album": "Doble Vida",
-        "genero": "Rock",
-        "anio": 1988,
-        "duracion_seg": 351
-    },
-    {
-        "id": "4",
-        "titulo": "Crimen",
-        "artista": "Gustavo Cerati",
-        "album": "Ahí vamos",
-        "genero": "Rock",
-        "anio": 2006,
-        "duracion_seg": 239
-    },
-    {
-        "id": "5",
-        "titulo": "Adios",
-        "artista": "Gustavo Cerati",
-        "album": "Fuerza Natural",
-        "genero": "Rock",
-        "anio": 2009,
-        "duracion_seg": 233
-    }
+    {"id": "1", "titulo": "De Musica Ligera", "artista": "Soda Stereo", "album": "Cancion Animal", "genero": "Rock", "anio": 1990, "duracion_seg": 213},
+    {"id": "2", "titulo": "Persiana Americana", "artista": "Soda Stereo", "album": "Signos", "genero": "Rock", "anio": 1986, "duracion_seg": 263},
+    {"id": "3", "titulo": "En la Ciudad de la Furia", "artista": "Soda Stereo", "album": "Doble Vida", "genero": "Rock", "anio": 1988, "duracion_seg": 351},
+    {"id": "4", "titulo": "Crimen", "artista": "Gustavo Cerati", "album": "Ahí vamos", "genero": "Rock", "anio": 2006, "duracion_seg": 239},
+    {"id": "5", "titulo": "Adios", "artista": "Gustavo Cerati", "album": "Fuerza Natural", "genero": "Rock", "anio": 2009, "duracion_seg": 233},
+    {"id": "12", "titulo": "Jijiji", "artista": "Patricio Rey y sus Redonditos de Ricota", "album": "Un Baion para el Ojo Idiota", "genero": "Rock", "anio": 1988, "duracion_seg": 316},
+    {"id": "13", "titulo": "Ji Ji Ji", "artista": "Patricio Rey y sus Redonditos de Ricota", "album": "En Directo", "genero": "Rock", "anio": 1992, "duracion_seg": 318},
+    {"id": "16", "titulo": "Gracias a la Vida", "artista": "Mercedes Sosa", "album": "Hasta la Victoria", "genero": "Folklore", "anio": 1971, "duracion_seg": 271},
+    {"id": "19", "titulo": "Flaca", "artista": "Andres Calamaro", "album": "Honestidad Brutal", "genero": "Rock", "anio": 1999, "duracion_seg": 263},
+    {"id": "31", "titulo": "Blank Space", "artista": "Taylor Swift", "album": "1989", "genero": "Pop", "anio": 2014, "duracion_seg": 231},
+    {"id": "32", "titulo": "Bohemian Rhapsody", "artista": "Queen", "album": "A Night at the Opera", "genero": "Rock", "anio": 1975, "duracion_seg": 354},
+    {"id": "33", "titulo": "Bohemian Rhapsody (Live Aid)", "artista": "Queen", "album": "Live Aid", "genero": "Rock", "anio": 1985, "duracion_seg": 362},
+    {"id": "37", "titulo": "Get Lucky", "artista": "Daft Punk", "album": "Random Access Memories", "genero": "Electronica", "anio": 2013, "duracion_seg": 248},
+    {"id": "47", "titulo": "Wonderwall", "artista": "Oasis", "album": "What's the Story Morning Glory", "genero": "Rock", "anio": 1995, "duracion_seg": 258},
+    {"id": "48", "titulo": "Wonderwall (Live)", "artista": "Oasis", "album": "Familiar to Millions", "genero": "Rock", "anio": 2000, "duracion_seg": 265},
+    {"id": "51", "titulo": "Billie Jean", "artista": "Michael Jackson", "album": "Thriller", "genero": "Pop", "anio": 1982, "duracion_seg": 294},
+    {"id": "52", "titulo": "Billie Jean (Remix)", "artista": "Michael Jackson", "album": "Thriller 40", "genero": "Pop", "anio": 2022, "duracion_seg": 292},
+    {"id": "54", "titulo": "Yellow", "artista": "Coldplay", "album": "Parachutes", "genero": "Rock", "anio": 2000, "duracion_seg": 269},
+    {"id": "55", "titulo": "Creep", "artista": "Radiohead", "album": "Pablo Honey", "genero": "Rock", "anio": 1992, "duracion_seg": 238},
+    {"id": "61", "titulo": "Gracias a la Vida", "artista": "Violeta Parra", "album": "Las Ultimas Composiciones", "genero": "Folklore", "anio": 1966, "duracion_seg": 258},
+    {"id": "62", "titulo": "De Musica Ligera (Unplugged)", "artista": "Soda Stereo", "album": "Comfort y Musica Para Volar", "genero": "Rock", "anio": 1996, "duracion_seg": 221},
+    {"id": "63", "titulo": "Flaca (En vivo)", "artista": "Andres Calamaro", "album": "Vivo", "genero": "Rock", "anio": 2000, "duracion_seg": 270},
+    {"id": "64", "titulo": "Creep (Live)", "artista": "Radiohead", "album": "The Astoria London", "genero": "Rock", "anio": 1994, "duracion_seg": 245},
+    {"id": "65", "titulo": "Yellow (Live)", "artista": "Coldplay", "album": "Live 2003", "genero": "Rock", "anio": 2003, "duracion_seg": 280},
+    {"id": "66", "titulo": "Blank Space (Acoustic)", "artista": "Taylor Swift", "album": "1989", "genero": "Pop", "anio": 2014, "duracion_seg": 228},
+    {"id": "67", "titulo": "Get Lucky (Daft Punk Remix)", "artista": "Daft Punk", "album": "Random Access Memories", "genero": "Electronica", "anio": 2013, "duracion_seg": 248}
 ]
 
 def obtener_catalogo():
